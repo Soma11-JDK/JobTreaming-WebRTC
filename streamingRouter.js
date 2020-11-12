05c76925af2ae63b7638a73e77d60fd4c91f27ac
김지영 <rlawldud335@naver.com>
2020-11-12 09:12:28 +0900

    exception2

--- streamingRouter.js
+++ streamingRouter.js
@@ -7,45 +7,46 @@ const streamingRouter = express.Router();
 const getStreaming = (req, res) => {
     //lectureId, password, jwt 받음.
     if (!req.query.lectureId && !req.headers.jwt) {
-        res.status(500).end();
-    }
-    const lectureId = req.query.lectureId;
-    const jwt = req.headers.jwt;
+        res.status(200).end();
+    } else {
+        const lectureId = req.query.lectureId;
+        const jwt = req.headers.jwt;
 
-    const password = makePassword(lectureId);
-    var headers = { 'Authorization': 'Bearer ' + jwt };
-    var options = {
-        url: 'http://117.16.136.156:8085/lecture/join',
-        method: 'GET',
-        headers: headers,
-        qs: { 'password': password, 'lectureId': lectureId }
-    };
-    request(options, function (error, response, body) {
-        if (error) console.log(error);
-        else if (!error && response.statusCode == 200) {
-            console.log('200');
-            body = JSON.parse(body);
-            const { name, expert } = body;
-            request(`http://117.16.136.156:8085/lecture/${lectureId}`, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    body = JSON.parse(body);
-                    let { title, category, startedAt, endedAt } = body;
-                    startedAt = startedAt.substring(11, 16);
-                    endedAt = endedAt.substring(11, 16);
-                    if (expert) {
-                        res.render("streamer", { userName: name, roomName: lectureId, streamer: true, title, category, startedAt, endedAt });
-                    } else {
-                        res.render("viewer", { userName: name, roomName: lectureId, streamer: false, title, category, startedAt, endedAt });
+        const password = makePassword(lectureId);
+        var headers = { 'Authorization': 'Bearer ' + jwt };
+        var options = {
+            url: 'http://117.16.136.156:8085/lecture/join',
+            method: 'GET',
+            headers: headers,
+            qs: { 'password': password, 'lectureId': lectureId }
+        };
+        request(options, function (error, response, body) {
+            if (error) console.log(error);
+            else if (!error && response.statusCode == 200) {
+                console.log('200');
+                body = JSON.parse(body);
+                const { name, expert } = body;
+                request(`http://117.16.136.156:8085/lecture/${lectureId}`, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        body = JSON.parse(body);
+                        let { title, category, startedAt, endedAt } = body;
+                        startedAt = startedAt.substring(11, 16);
+                        endedAt = endedAt.substring(11, 16);
+                        if (expert) {
+                            res.render("streamer", { userName: name, roomName: lectureId, streamer: true, title, category, startedAt, endedAt });
+                        } else {
+                            res.render("viewer", { userName: name, roomName: lectureId, streamer: false, title, category, startedAt, endedAt });
+                        }
                     }
-                }
-            });
-        }
-        else {
-            //강연참여자가 아님
-            console.log('500');
-            res.status(500);
-        }
-    });
+                });
+            }
+            else {
+                //강연참여자가 아님
+                console.log('500');
+                res.status(500);
+            }
+        });
+    }
 }
 
 streamingRouter.get("/", getStreaming);
