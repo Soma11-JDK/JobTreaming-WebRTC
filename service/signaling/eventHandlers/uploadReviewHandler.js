23a3a377575cf2e0f539e46826e11733ad519c2c
김지영 <rlawldud335@naver.com>
2020-11-17 04:38:18 +0900

    options

--- service/signaling/eventHandlers/uploadReviewHandler.js
+++ service/signaling/eventHandlers/uploadReviewHandler.js
@@ -3,45 +3,45 @@ import EVENT from "../../event";
 import request from "request";
 
 const uploadReviewHandler = (socket, data) => {
-    request.post(
-        {
-            url: 'http://117.16.136.156:8085/review/add',
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-                "Authorization": data.Authorization
-            },
-            body: {
-                "lecture": data.lecture,
-                "contents": data.contents,
-                "rating": data.rating
-            },
-            json: true
-        }, function (error, response, body) {
-            console.log(error);
-            console.log(response, body);
-        }
+    const options = {
+        url: 'http://117.16.136.156:8085/review/add',
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "Authorization": data.Authorization
+        },
+        body: {
+            "lecture": data.lecture,
+            "contents": data.contents,
+            "rating": data.rating
+        },
+        json: true
+    };
+
+    request.post(options, function (error, response, body) {
+        console.log(error, response.statusCode);
+    }
     );
 }
 
 const uploadExpertHandler = (socket, data) => {
-    request.post(
-        {
-            url: 'http://117.16.136.156:8085/evaluation/add',
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-                "Authorization": data.Authorization
-            },
-            body: {
-                "expertId": data.expertId,
-                "evaluation": data.evaluation
-            },
-            json: true
-        }, function (error, response, body) {
-            console.log(error);
-            console.log(response, body);
-        }
+    const options = {
+        url: 'http://117.16.136.156:8085/evaluation/add',
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "Authorization": data.Authorization
+        },
+        body: {
+            "expertId": data.expertId,
+            "evaluation": data.evaluation
+        },
+        json: true
+    };
+
+    request.post(options, function (error, response, body) {
+        console.log(error, response.statusCode);
+    }
     );
 }
 
